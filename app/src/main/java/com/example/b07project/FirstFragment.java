423dc56dea1026b078c89e3d49c980f5e4d27296
MuhannadJam <muhannad.aljamali@mail.utoronto.ca>
2022-11-24 18:54:29 -0500

    Modified FirstFragment
    Fixed a bug that was causing the app to crash

--- app/src/main/java/com/example/b07project/FirstFragment.java
+++ app/src/main/java/com/example/b07project/FirstFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -30,6 +31,13 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_signupFragment);
+            }
+        });
     }
 
     @Override
