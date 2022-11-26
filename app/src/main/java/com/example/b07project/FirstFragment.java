92f9ebd6ac6dcdaa94d6ea46dd271e7562b8290d
MuhannadJam <muhannad.aljamali@mail.utoronto.ca>
2022-11-26 17:41:30 -0500

    Update FirstFragment
    To fix a crash issue

--- app/src/main/java/com/example/b07project/FirstFragment.java
+++ app/src/main/java/com/example/b07project/FirstFragment.java
@@ -1,9 +1,11 @@
 package com.example.b07project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.b07project.databinding.FragmentFirstBinding;
+
+import java.io.BufferedReader;
 
 public class FirstFragment extends Fragment {
 
@@ -23,19 +27,45 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        Button login_button = (Button) view.findViewById(R.id.login);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+            }
+        });
+
+        return view;
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.signup.setOnClickListener(new View.OnClickListener() {
+
+        view.findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_signupFragment);
+            }
+        });
+
+        EditText username = (EditText) view.findViewById(R.id.usernameInput);
+        view.findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("name")){
+                    NavHostFragment.findNavController(FirstFragment.this
+                    ).navigate(R.id.action_FirstFragment_to_SecondFragment);
+                }
+                else{
+                    Toast.makeText(view.getContext(), "Invalid Credentials!",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
