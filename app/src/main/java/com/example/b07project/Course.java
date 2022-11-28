5c7b22845eb638a3ab7df47471cd5659778944b2
MuhannadJam <muhannad.aljamali@mail.utoronto.ca>
2022-11-28 18:50:52 -0500

    Update Course.java
    Renamed the variables

--- app/src/main/java/com/example/b07project/Course.java
+++ app/src/main/java/com/example/b07project/Course.java
@@ -3,15 +3,15 @@ package com.example.b07project;
 import java.util.ArrayList;
 
 public class Course {
-        String str_name;
-        String str_code;
-        char char_session; //F, W, S for fall, winter and summer
-        ArrayList<String> arrlst_prereq = new ArrayList<String>();
+        String name;
+        String code;
+        char session; //F, W, S for fall, winter and summer
+        ArrayList<String> prereq;
 
-    public Course(String str_name, String str_code, char char_session, ArrayList<String> arrlst_prereq) {
-        this.str_name = str_name;
-        this.str_code = str_code;
-        this.char_session = char_session;
-        this.arrlst_prereq = arrlst_prereq;
+    public Course(String name, String code, char session, ArrayList<String> prereq) {
+        this.name = name;
+        this.code = code;
+        this.session = session;
+        this.prereq.addAll(prereq);
     }
 }
