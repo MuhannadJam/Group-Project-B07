8744d3afad5196abaf222fc59e6f976f2326fbee
hamimk7889 <118387373+hamimk7889@users.noreply.github.com>
2022-11-30 09:26:19 -0500

    Made Changes So Course Now has an array list of courses containing its prereq Made Changes to Student so now there are two lists associated to it courses taken and courses planned Added GenerateTimetable so Student can see a timetable after adding the courses they planned

--- app/src/main/java/com/example/b07project/Course.java
+++ app/src/main/java/com/example/b07project/Course.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 public class Course {
     public String name;
     public String code;
-    public String session; //F, W, S for fall, winter and summer
-    public ArrayList<String> prereq = new ArrayList<>();
+    public String level; //A, B, C respectively perhaps uneccesary because could use 4th char in code string
+    public ArrayList<String> session= new ArrayList<>(); //F, W, S for fall, winter and summer
+    public ArrayList<Course> prereq = new ArrayList<>();
 
-    public Course(String name, String code, String session, ArrayList<String> prereq) {
+    public Course(String name, String code, String level, ArrayList<String> session, ArrayList<Course> prereq) {
         this.name = name;
         this.code = code;
-        this.session = session;
+        this.level = level;
+        this.session.addAll(session);
         this.prereq.addAll(prereq);
     }
 }
--- app/src/main/java/com/example/b07project/Student.java
+++ app/src/main/java/com/example/b07project/Student.java
@@ -7,7 +7,8 @@ public class Student {
     public String name;
     public String email;
 
-    public ArrayList <String> courses= new ArrayList<String>();
+    public ArrayList <Course> coursesTaken= new ArrayList<Course>();
+    public ArrayList <Course> coursesPlanned= new ArrayList<Course>();
 
     static ArrayList<Student> students = new ArrayList<Student>();
 
@@ -15,10 +16,56 @@ public class Student {
 
     }
 
-    public Student(String name, String email, ArrayList courses) {
+    public Student(String name, String email, ArrayList coursesTaken) {
         this.name = name;
         this.email = email;
-        this.courses.addAll(courses);
+        this.coursesTaken.addAll(coursesTaken);
+
+    }
+
+    public ArrayList<Course> generateTimetable(Student s) {
+        ArrayList<Course> timetable = new ArrayList<Course>();
+        for (int i = 0; i < s.coursesPlanned.size(); i++) {
+            for (int k = 0; k < s.coursesTaken.size(); k++) {
+                if (!(s.coursesTaken.contains(s.coursesPlanned.get(i).prereq.get(k)))) {
+                    if (!(s.coursesPlanned.contains(s.coursesPlanned.get(i).prereq.get(k)))) {
+                        s.coursesPlanned.add((s.coursesPlanned.get(i).prereq.get(k)));
+                    }
+                }
+            }
+        }
+        int j = s.coursesPlanned.size();
+        while (j > 0) {
+            for (int i =0; i < s.coursesPlanned.size(); i++) {
+                if ((s.coursesPlanned.get(i).prereq.isEmpty() || s.coursesPlanned.get(i).prereq.containsAll(s.coursesTaken)) && (s.coursesPlanned.get(i).session.contains("F"))) {
+                    //assume that user is making a timetable before Fall Session
+                    s.coursesTaken.add(s.coursesPlanned.get(i));//
+                    timetable.add(s.coursesPlanned.get(i));
+                    s.coursesPlanned.remove(s.coursesPlanned.get(i));
+                    j--;
+                }
+            }
+            for (int i =0; i < s.coursesPlanned.size(); i++) {
+                if ((s.coursesPlanned.get(i).prereq.isEmpty() || s.coursesPlanned.get(i).prereq.containsAll(s.coursesTaken)) && (s.coursesPlanned.get(i).session.contains("W"))) {
+                    s.coursesTaken.add(s.coursesPlanned.get(i));
+                    timetable.add(s.coursesPlanned.get(i));
+                    s.coursesPlanned.remove(s.coursesPlanned.get(i));
+                    j--;
+                }
+            }
+            for (int i =0; i < s.coursesPlanned.size(); i++) {
+                if ((s.coursesPlanned.get(i).prereq.isEmpty() || s.coursesPlanned.get(i).prereq.containsAll(s.coursesTaken)) && (s.coursesPlanned.get(i).session.contains("S"))) {
+                    s.coursesTaken.add(s.coursesPlanned.get(i));
+                    timetable.add(s.coursesPlanned.get(i));
+                    s.coursesPlanned.remove(s.coursesPlanned.get(i));
+                    j--;
+                }
+            }
+
+        }
+        return timetable;
+    }
+
 
     }
 
