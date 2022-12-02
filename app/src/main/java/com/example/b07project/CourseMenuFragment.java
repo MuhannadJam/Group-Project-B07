d15382eda59fcd7cd056a0d4900729ca8ac9ac09
MuhannadJam <muhannad.aljamali@mail.utoronto.ca>
2022-12-02 11:30:23 -0500

    Update CourseMenuFragment.java
    Students can now see the courses they have taken and the courses they can add

--- app/src/main/java/com/example/b07project/CourseMenuFragment.java
+++ app/src/main/java/com/example/b07project/CourseMenuFragment.java
@@ -10,47 +10,131 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class CourseMenuFragment extends Fragment {
+
     String[] items = {"BO7", "BO7", "BO7", "BO7", "BO7", "BO7", "BO7"};
     View view;
 
-    public CourseMenuFragment() {
-        // Required empty public constructor
-    }
+    ListView courseTakenList;
+    ListView courseAddList;
 
+    ArrayList <String> courses;
+    ArrayList <Course> coursesTaken;
+    ArrayList <String> courseTakenDisplay;
+    ArrayList <String> addCoursesDisplay;
 
+    ImageView backButton;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    DatabaseReference cRef;
+    DatabaseReference cTakenRef;
+    FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         view = inflater.inflate(R.layout.course_menu, container, false);
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, items);
-        ListView listView = (ListView) view.findViewById(R.id.course_taken_list);
-        listView.setAdapter(itemsAdapter);
-        ArrayAdapter<String> itemsAdapter2 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, items);
-        ListView listView2 = (ListView) view.findViewById(R.id.add_courses_list);
-        listView2.setAdapter(itemsAdapter);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        cRef = FirebaseDatabase.
+                getInstance("https://bo7-project-default-rtdb.firebaseio.com/").
+                getReference("Courses");
+
+        cTakenRef = FirebaseDatabase.
+                getInstance("https://bo7-project-default-rtdb.firebaseio.com/").
+                getReference("Students").child(mAuth.getCurrentUser().getUid()).
+                child("coursesTaken");
+
+        backButton = (ImageView) view.findViewById(R.id.back_button);
+
+        coursesTaken = new ArrayList<Course>();
+        courses = new ArrayList<String>();
+        courseTakenDisplay = new ArrayList<>();
+        addCoursesDisplay = new ArrayList<>();
+
+        courseTakenList = (ListView) view.findViewById(R.id.course_taken_list);
+        courseAddList = (ListView) view.findViewById(R.id.add_courses_list);
+
         return view;
     }
+
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
+
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(CourseMenuFragment.this)
                         .navigate(R.id.action_course_menu_to_SecondFragment);
             }
         });
+
+        cTakenRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DataSnapshot child: task.getResult().getChildren()) {
+                        Course course = child.getValue(Course.class);
+                        coursesTaken.add(course);
+                        courseTakenDisplay.add(course.code);
+                    }
+
+                    ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getContext(),
+                            android.R.layout.simple_list_item_1, courseTakenDisplay);
+
+                    courseTakenList.setAdapter(itemsAdapter);
+                }
+                else {
+                    return;
+                }
+            }
+        });
+
+        cRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DataSnapshot child: task.getResult().getChildren()) {
+                        Course course = child.getValue(Course.class);
+                        courses.add(course.code);
+                    }
+
+                    for (String course: courses) {
+                        if (!(coursesTaken.contains(course))) {
+                            addCoursesDisplay.add(course);
+                        }
+                    }
+
+                    ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getContext(),
+                            android.R.layout.simple_list_item_1, addCoursesDisplay);
+
+                    courseAddList.setAdapter(itemsAdapter);
+                }
+                else {
+                    return;
+                }
+            }
+        });
+
+
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
