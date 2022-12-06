fee0d5b0a0eeed7b6c62429ba29d3dbf31d507c4
chaneth8 <83342511+chaneth8@users.noreply.github.com>
2022-12-05 19:16:51 -0500

    Finished off test cases

--- app/src/test/java/com/example/b07project/ExampleUnitTest.java
+++ app/src/test/java/com/example/b07project/ExampleUnitTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.util.Patterns;
 import android.widget.EditText;
 
 /**
@@ -28,6 +29,9 @@ public class ExampleUnitTest {
     @Mock
     EditText email;
 
+    @Mock
+    EditText password;
+
     @Test
     public void testEmptyEmail() {
         when(view.getEmail()).thenReturn("");
@@ -39,14 +43,36 @@ public class ExampleUnitTest {
     }
 
     @Test
+    public void testEmailPattern() {
+        when(view.getEmail()).thenReturn("abc");
+        when(view.checkPattern("abc")).thenReturn(false);
+        when(view.getEmailField()).thenReturn(email);
+
+        Presenter presenter = new Presenter(model, view);
+        presenter.checkEmail();
+        verify(view).displayMessage(email, "Invalid Email");
+    }
+
+    @Test
     public void testInvalidEmail() {
         when(view.getEmail()).thenReturn("abc@gmail.com");
+        when(view.checkPattern("abc@gmail.com")).thenReturn(true);
         when(model.isFound("abc@gmail.com")).thenReturn(false);
         when(view.getEmailField()).thenReturn(email);
 
         Presenter presenter = new Presenter(model, view);
         presenter.checkEmail();
         verify(view).displayMessage(email, "Email not registered");
+    }
+
+    @Test
+    public void testEmptyPassword() {
+        when(view.getPassword()).thenReturn("");
+        when(view.getPasswordField()).thenReturn(password);
+
+        Presenter presenter = new Presenter(model, view);
+        presenter.checkPassword();
+        verify(view).displayMessage(password, "Password Required");
     }
 
 }
--- app/src/test/java/com/example/b07project/MainActivityTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.b07project;
-
-import junit.framework.TestCase;
-
-public class MainActivityTest extends TestCase {
-
-    public void testOnCreate() {
-    }
-}
